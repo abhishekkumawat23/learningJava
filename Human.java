6e10a336d9444f17a3d551bfd0aff41dd68428df
Abhishek Kumawat <abhishekkumawat23@gmail.com>
2020-10-07 19:45:32 +0200

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,35 @@
+// Clas is a template
+public class Human {
+    // Methods / Functions
+    // Properties / Variables
+    // Constructor
+
+    public int age = 0;
+    public String firstName = "";
+    public String lastName = "";
+    public String color = "";
+
+    // Contructor is also a method, but a special kind of method.
+
+    // Constructor - name should be same as class name.
+    // first, last, meriAge, meraColor are parameters of constructor.
+    public Human(String first, String last, int meriAge, String meraColor) {
+        this.firstName = first;
+        this.lastName = last;
+        this.age = meriAge;
+        this.color = meraColor;
+    }
+
+    // Methods
+    public void eat() {
+       // Eat here. 
+    }
+
+    public void sleep() {
+        // Sleep
+    }
+
+    public void walk() {
+        // Walk.
+    }
+}
