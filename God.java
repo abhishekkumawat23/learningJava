63c4500a8f1f0c503977cd60058cb34bcbde4773
Abhishek Kumawat <akumawat@google.com>
2020-10-07 19:43:15 +0200

    God first commit

---
+++ God.java
@@ -0,0 +1,9 @@
+public class God {
+
+    public static void main(String[] args) {
+        Human chinu = new Human("Saloni", "Kumawat", 25, "White"); // Chinu is a object of Human class.
+        Human mehul = new Human("Mehul", "Kumawat", 21, "Black"); // Mehul is a object of Human class.
+
+        System.out.println("Chinu name in Human world is: " + chinu.firstName + " " + chinu.lastName);
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(Sttring[] args) {
+        System.out.println("Hello World");
+    }
+}
